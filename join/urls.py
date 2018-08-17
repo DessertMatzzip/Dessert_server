818420879ac16f60b8be8a141aae6350da5097c2
markim94 <markim94@naver.com>
2018-08-17 14:23:13 +0900

    added views def 'joinkakao, joinfacebook' and modified urls.py

--- join/urls.py
+++ join/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('user', views.join, name='join'),
+    path('itself', views.joinItself, name='joinItself'),
+    path('kakao', views.joinKakao, name='joinKakao'),
+    path('facebook', views.joinFacebook, name='joinFacebook'),
     path('modify', views.modify, name='modify'),
 ]
